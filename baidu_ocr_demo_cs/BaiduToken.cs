87bfb2c31a0b470336cda01104c96c0d11206a3b
sorrymaker <1842136158@qq.com>
2022-08-09 14:50:33 +0800

    添加项目文件。

---
+++ baidu_ocr_demo_cs/BaiduToken.cs
@@ -0,0 +1,28 @@
+//如果好用，请收藏地址，帮忙分享。
+public class BaiduToken
+{
+    /// &lt;summary&gt;
+    /// 
+    /// &lt;/summary&gt;
+    public string refresh_token { get; set; }
+    /// &lt;summary&gt;
+    /// 
+    /// &lt;/summary&gt;
+    public int expires_in { get; set; }
+    /// &lt;summary&gt;
+    /// 
+    /// &lt;/summary&gt;
+    public string session_key { get; set; }
+    /// &lt;summary&gt;
+    /// 
+    /// &lt;/summary&gt;
+    public string access_token { get; set; }
+    /// &lt;summary&gt;
+    /// public vis-ocr_ocr brain_ocr_scope brain_ocr_general brain_ocr_general_basic vis-ocr_business_license brain_ocr_webimage brain_all_scope brain_ocr_idcard brain_ocr_driving_license brain_ocr_vehicle_license vis-ocr_plate_number brain_solution brain_ocr_plate_number brain_ocr_accurate brain_ocr_accurate_basic brain_ocr_receipt brain_ocr_business_license brain_solution_iocr brain_qrcode brain_ocr_handwriting brain_form brain_ocr_passport brain_ocr_vat_invoice brain_numbers brain_ocr_business_card brain_ocr_train_ticket brain_ocr_taxi_receipt vis-ocr_household_register vis-ocr_vis-classify_birth_certificate vis-ocr_台湾通行证 vis-ocr_港澳通行证 vis-ocr_机动车购车发票识别 vis-ocr_机动车检验合格证识别 vis-ocr_车辆vin码识别 vis-ocr_定额发票识别 vis-ocr_保单识别 vis-ocr_机打发票识别 vis-ocr_行程单识别 brain_ocr_vin brain_ocr_quota_invoice brain_ocr_birth_certificate brain_ocr_household_register brain_ocr_HK_Macau_pass brain_ocr_taiwan_pass brain_ocr_vehicle_invoice brain_ocr_vehicle_certificate brain_ocr_air_ticket brain_ocr_invoice brain_ocr_insurance_doc brain_formula brain_seal brain_ocr_facade brain_ocr_meter brain_doc_analysis brain_ocr_webimage_loc brain_ocr_medical_paper brain_ocr_doc_analysis_office brain_vat_invoice_verification brain_ocr_used_vehicle_invoice brain_ocr_medical_detail brain_vehicle_registration_certificate brain_ocr_online_taxi_itinerary brain_ocr_multi_idcard brain_ocr_mixed_multi_vehicle brain_ocr_weigth_note brain_ocr_ multiple_invoice brain_ocr_medical_report_detection brain_ocr_waybill brain_ocr_road_transport_certificate brain_form_table brain_ocr_health_code brain_ocr_covid_test wise_adapt lebo_resource_base lightservice_public hetu_basic lightcms_map_poi kaidian_kaidian ApsMisTest_Test权限 vis-classify_flower lpq_开放 cop_helloScope ApsMis_fangdi_permission smartapp_snsapi_base smartapp_mapp_dev_manage iop_autocar oauth_tp_app smartapp_smart_game_openapi oauth_sessionkey smartapp_swanid_verify smartapp_opensource_openapi smartapp_opensource_recapi fake_face_detect_开放Scope vis-ocr_虚拟人物助理 idl-video_虚拟人物助理 smartapp_component smartapp_search_plugin avatar_video_test b2b_tp_openapi b2b_tp_openapi_online smartapp_gov_aladin_to_xcx
+    /// &lt;/summary&gt;
+    public string scope { get; set; }
+    /// &lt;summary&gt;
+    /// 
+    /// &lt;/summary&gt;
+    public string session_secret { get; set; }
+}
---
+++ baidu_ocr_demo_cs/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace baidu_ocr_demo_cs
+{
+    public static class AccessToken
+
+    {
+        // 调用getAccessToken()获取的 access_token建议根据expires_in 时间 设置缓存
+        // 返回token示例
+        // public static String TOKEN = "24.adda70c11b9786206253ddb70affdc46.2592000.1493524354.282335-1234567";
+
+        // 百度云中开通对应服务应用的 API Key 建议开通应用的时候多选服务
+        private static String clientId = "d8jNcYsMxNu4GeTTkWFlNouF";
+        // 百度云中开通对应服务应用的 Secret Key
+        private static String clientSecret = "SbyTO7NutbfhfWyON2VQa4C42fjWPz6P";
+
+        public static String getAccessToken()
+        {
+            String authHost = "https://aip.baidubce.com/oauth/2.0/token";
+            HttpClient client = new HttpClient();
+            List<KeyValuePair<String, String>> paraList = new List<KeyValuePair<string, string>>();
+            paraList.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            paraList.Add(new KeyValuePair<string, string>("client_id", clientId));
+            paraList.Add(new KeyValuePair<string, string>("client_secret", clientSecret));
+
+            HttpResponseMessage response = client.PostAsync(authHost, new FormUrlEncodedContent(paraList)).Result;
+            String result = response.Content.ReadAsStringAsync().Result;
+
+            // Newtonsoft转换json数据
+            String token = Newtonsoft.Json.JsonConvert.DeserializeObject<BaiduToken>(result).access_token;
+            Console.WriteLine(token);
+            return token;
+        }
+    }
+    public class General
+    {
+        // OCR 通用识别
+        public static string general()
+        {
+            string token = AccessToken.getAccessToken();
+            string host = "https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=" + token;
+            Encoding encoding = Encoding.Default;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host);
+            request.Method = "post";
+            request.KeepAlive = true;
+            // 图片的base64编码
+            string base64 = getFileBase64("img/test.png");
+            String str = "image=" + HttpUtility.UrlEncode(base64);
+            byte[] buffer = encoding.GetBytes(str);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.Default);
+            string result = reader.ReadToEnd();
+            Console.WriteLine("OCR 通用识别:");
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static String getFileBase64(String fileName)
+        {
+            FileStream filestream = new FileStream(fileName, FileMode.Open);
+            byte[] arr = new byte[filestream.Length];
+            filestream.Read(arr, 0, (int)filestream.Length);
+            string baser64 = Convert.ToBase64String(arr);
+            filestream.Close();
+            return baser64;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            General.general();
+        }
+    }
+}
